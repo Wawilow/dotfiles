1a7ff9c3fc076a8b50dbaed6a6531e5cb674ff95
wawilow <wawilow@protonmail.com>
2026-02-05 19:26:25 -0500

    Add plugin to move lines around and easily surround stuff

---
+++ nvim/lua/plugins/mini.surround.lua
@@ -0,0 +1,14 @@
+return {
+  "nvim-mini/mini.surround",
+  opts = {
+    mappings = {
+      add = "<leader>f", -- Add surrounding in Normal and Visual modes
+      delete = "<leader>fd", -- Delete surrounding
+      find = "<leader>ff", -- Find surrounding
+      find_left = "<leader>fF", -- Find surrounding left
+      highlight = "<leader>fh", -- Highlight surrounding
+      replace = "<leader>fr", -- Replace surrounding
+      update_n_lines = "<leader>fn", -- Update n_lines
+    },
+  },
+}
