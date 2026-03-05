aed5e3f8afd617a1e85d984d507561bb9fca0e0a
wawilow <wawilow@protonmail.com>
2026-03-05 13:07:46 -0500

    Ignore some useless stuff + import with ruff

--- nvim/lua/plugins/python.lua
+++ nvim/lua/plugins/python.lua
@@ -4,12 +4,34 @@ return {
     opts = {
       servers = {
         pyright = { enabled = false },
+        python = {
+          analysis = {
+            ignore = { "*" },
+          },
+        },
         basedpyright = {
           enabled = true,
+          disableOrganizeImports = true,
           settings = {
             basedpyright = {
               analysis = {
                 typeCheckingMode = "off",
+                diagnosticMode = "openFilesOnly",
+                exclude = {
+                  "**/node_modules",
+                  "**/__pycache__",
+                  "**/env",
+                  "**/.env",
+                  "**/site-packages",
+                  "**/build",
+                  "**/dist",
+                  "**/.eggs",
+                  "**/*.egg-info",
+                  "**/.pytest_cache",
+                  "**/.mypy_cache",
+                  "**/.ruff_cache",
+                  "**/.git",
+                },
               },
             },
           },
