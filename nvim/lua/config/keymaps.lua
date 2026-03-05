e2399583efd8cc1090a39263fdafcd453c2c9ea5
wawilow <wawilow@protonmail.com>
2026-03-05 13:07:02 -0500

    Better keymaps

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -2,6 +2,10 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
+vim.keymap.del("n", "<leader>l")
+vim.keymap.del("n", "<leader>L")
+vim.keymap.del("n", "<leader>K")
+
 -- close current buffer with ctrl + x
 vim.keymap.set("n", "<C-x>", function()
   Snacks.bufdelete()
@@ -10,31 +14,26 @@ end, { desc = "Delete Buffer" })
 -- tabs stuff
 vim.keymap.set("n", "<Tab>", "<cmd>bnext<cr>", { desc = "Next Buffer" })
 vim.keymap.set("n", "<S-Tab>", "<cmd>bprev<cr>", { desc = "Prev Buffer" })
--- to close window type <Crtl+w> c
+
+vim.keymap.set("n", "<leader>f/", LazyVim.pick("live_grep"), { desc = "Grep (Root Dir)" })
+vim.keymap.set("n", "<leader>f.", function()
+  Snacks.picker.grep({ cwd = vim.fn.getcwd() })
+end, { desc = "Grep (CWD)" })
 
 -- comment
-vim.keymap.set("n", "<leader>f/", LazyVim.pick("live_grep"), { desc = "Grep (Root Dir)" })
 vim.keymap.del("n", "<leader>/") -- unmap so I could map comment one here
 vim.keymap.set("n", "<leader>/", "gcc", { remap = true, desc = "Toggle Comment" })
 vim.keymap.set("v", "<leader>/", "gc", { remap = true, desc = "Toggle Comment" })
 
 -- reload LSP
-vim.keymap.set("n", "<leader>cR", ":LspRestart<CR>", { desc = "Restart LSP" })
+vim.keymap.set("n", "<leader>cV", ":LspRestart<CR>", { desc = "Restart LSP" })
 
 -- settings for diffview.nvim plugin
--- vim.keymap.set(
---   "n",
---   "<leader>g/",
---   ":DiffviewOpen<CR>",
---   { desc = "Show diff on current file", noremap = true, silent = true }
--- )
--- vim.keymap.set("n", "<leader>g|", ":DiffviewToggleFiles<CR>", { noremap = true, silent = true })
-vim.keymap.set("n", "<leader>gd", ":DiffviewOpen<CR>", { desc = "Open Diffview" })
-vim.keymap.set("n", "<leader>gD", ":DiffviewToggleFiles<CR>", { desc = "Toggle Diffview Files" })
-
-vim.api.nvim_create_user_command("DebugKeymaps", function()
-  local gd = vim.fn.maparg("<leader>gd", "n")
-  local gD = vim.fn.maparg("<leader>gD", "n")
-  print("✓ <leader>gd:", gd)
-  print("✓ <leader>gD:", gD)
-end, {})
+vim.keymap.set("n", "<leader>g/", function()
+  local view = require("diffview.lib").get_current_view()
+  if view then
+    vim.cmd("DiffviewClose")
+  else
+    vim.cmd("DiffviewOpen")
+  end
+end, { desc = "Toggle Diffview", silent = true })
