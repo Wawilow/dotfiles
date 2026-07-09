8a395f09c30da93dbcd2d22191cc0c2e8fdeb28d
wawilow <wawilow@protonmail.com>
2026-07-09 02:13:59 -0400

    Minor NVIM updates, including AI workflow plugins

--- nvim/lua/plugins/render-markdown.lua
+++ nvim/lua/plugins/render-markdown.lua
@@ -1,11 +1,12 @@
-return {}
--- I don't really like markdown redering rn...
--- return {
---   "MeanderingProgrammer/render-markdown.nvim",
---   dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-mini/mini.nvim" }, -- if you use the mini.nvim suite
---   -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.icons' },        -- if you use standalone mini plugins
---   -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
---   ---@module 'render-markdown'
---   ---@type render.md.UserConfig
---   opts = {},
--- }
+return {
+  "MeanderingProgrammer/render-markdown.nvim",
+  dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-mini/mini.nvim" }, -- if you use the mini.nvim suite
+  -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.icons' },        -- if you use standalone mini plugins
+  -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+  ---@module 'render-markdown'
+  ---@type render.md.UserConfig
+  opts = {
+    file_types = { "markdown", "Avante" },
+  },
+  ft = { "markdown", "Avante" },
+}
