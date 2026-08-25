a8497fd2605afa855092bdaf0ba0d0e5890eceac
wawilow <wawilow@protonmail.com>
2026-08-25 17:15:35 -0400

    opencode integrations

--- nvim/lua/plugins/ai.lua
+++ nvim/lua/plugins/ai.lua
@@ -1,70 +1,5 @@
--- (\(\
--- ( -.-)
--- o_(")(")
-
 local ok, secrets = pcall(require, "secrets")
 local claude_code_oauth_token = ok and secrets.claude_code_oauth_token
-
--- return {
---   "olimorris/codecompanion.nvim",
---   version = "^19.0.0",
---   opts = {
---   },
---   dependencies = {
---     "nvim-lua/plenary.nvim",
---     "nvim-treesitter/nvim-treesitter",
---   },
--- }
-
--- return {
---   "olimorris/codecompanion.nvim",
---   version = "^19.0.0",
---   opts = {
---     adapters = {
---       http = {
---         ["llama.cpp"] = function()
---           return require("codecompanion.adapters").extend("openai_compatible", {
---             env = {
---               -- make sure that llama docker is running
---               url = "http://127.0.0.1:11434",
---               api_key = "TERM",
---               chat_url = "/v1/chat/completions",
---             },
---             schema = {
---               model = {
---                 default = "llama3.1:8b",
---               },
---             },
---             handlers = {
---               parse_message_meta = function(self, data)
---                 local extra = data.extra
---                 if extra and extra.reasoning_content then
---                   data.output.reasoning = { content = extra.reasoning_content }
---                   if data.output.content == "" then
---                     data.output.content = nil
---                   end
---                 end
---                 return data
---               end,
---             },
---           })
---         end,
---       },
---     },
---     interactions = {
---       chat = {
---         adapter = "llama.cpp",
---       },
---       inline = {
---         adapter = "llama.cpp",
---       },
---     },
---   },
---   dependencies = {
---     "nvim-lua/plenary.nvim",
---     "nvim-treesitter/nvim-treesitter",
---   },
--- }
 
 return {
   "olimorris/codecompanion.nvim",
@@ -72,13 +7,13 @@ return {
   opts = {
     adapters = {
       acp = {
-        claude_code = function()
-          return require("codecompanion.adapters").extend("claude_code", {
-            env = {
-              CLAUDE_CODE_OAUTH_TOKEN = claude_code_oauth_token,
-            },
-          })
-        end,
+        -- claude_code = function()
+        --   return require("codecompanion.adapters").extend("claude_code", {
+        --     env = {
+        --       CLAUDE_CODE_OAUTH_TOKEN = claude_code_oauth_token,
+        --     },
+        --   })
+        -- end,
       },
     },
     rules = {
@@ -92,7 +27,8 @@ return {
 
     interactions = {
       chat = {
-        adapter = "claude_code",
+        -- adapter = "claude_code",
+        adapter = "opencode",
 
         tools = {
           ["run_command"] = {
@@ -120,10 +56,10 @@ return {
           },
         },
 
-        opts = {
-          -- this overrides default config, please be careful
-          system_prompt = "My new system prompt",
-        },
+        -- opts = {
+        --   -- this overrides default config, please be careful
+        --   system_prompt = "My new system prompt",
+        -- },
       },
       shared = {
         keymaps = {
